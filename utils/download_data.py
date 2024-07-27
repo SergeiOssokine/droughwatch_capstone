929b11da9a70dfe6a5fe62e993bd6280a7a4b64c
Serguei Ossokine <serguei.ossokine@tutanota.com>
2024-07-28 00:06:06 +0200

    Lint downloader

--- utils/download_data.py
+++ utils/download_data.py
@@ -1,3 +1,8 @@
+"""Quick script for downloading project data.
+See here:
+https://github.com/wandb/droughtwatch
+"""
+
 import logging
 import os
 import zipfile
@@ -23,8 +28,8 @@ logger.setLevel("CRITICAL")
 # Setup rich to get nice tracebacks
 install()
 
-data_url = "https://storage.googleapis.com/wandb_datasets/dw_train_86K_val_10K.zip"
-filepath = "./training/airflow/data/data.zip"
+DATA_URL = "https://storage.googleapis.com/wandb_datasets/dw_train_86K_val_10K.zip"
+FILEPATH = "./training/airflow/data/data.zip"
 
 
 def main(
@@ -32,15 +37,24 @@ def main(
         bool, typer.Option(help="Extract the data from the archive")
     ] = True,
     verbose: Annotated[bool, typer.Option(help="Provide verbose progress")] = True,
-):
+) -> None:
+    """Download the data from the droughtwatch benchmark
+
+    Args:
+        extract (Annotated[ bool, typer.Option, optional): Extract the data. Defaults to True.
+        verbose (Annotated[bool, typer.Option, optional): Verbose. Defaults to True.
+
+    Raises:
+        ValueError: If the download can't be completed
+    """
     if verbose:
         logger.setLevel("INFO")
-    response = requests.get(data_url, stream=True)
+    response = requests.get(DATA_URL, stream=True, timeout=20)
     if not response.ok:
-        logger.critical(f"Got response code {response.status_code} from {data_url}")
+        logger.critical(f"Got response code {response.status_code} from {DATA_URL}")
         raise ValueError
     logger.info("Downloading the training and validation data")
-    logger.info(f"URL: {data_url}")
+    logger.info(f"URL: {DATA_URL}")
     # Sizes in bytes.
     total_size = int(response.headers.get("content-length", 0))
     block_size = 1024
@@ -55,12 +69,12 @@ def main(
         "•",
         TimeRemainingColumn(),
     )
-    filename = os.path.basename(data_url)
+    filename = os.path.basename(DATA_URL)
     task_id = progress.add_task(
         "download", filename=filename, start=False, total=total_size
     )
     with progress:
-        with open(filepath, "wb") as dest_file:
+        with open(FILEPATH, "wb") as dest_file:
             progress.start_task(task_id)
             for data in response.iter_content(block_size):
                 size = dest_file.write(data)
@@ -68,9 +82,9 @@ def main(
 
     logger.info(f"Downloaded {filename}")
     if extract:
-        dest = os.path.dirname(filepath)
+        dest = os.path.dirname(FILEPATH)
         logger.info(f"Extracting archive to {dest}")
-        with zipfile.ZipFile(filepath, "r") as zip_ref:
+        with zipfile.ZipFile(FILEPATH, "r") as zip_ref:
             zip_ref.extractall(dest)
         logger.info("Done")
 
