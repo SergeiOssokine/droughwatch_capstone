af40748f72f79efc1292319acf9bbd1c05491116
Serguei Ossokine <serguei.ossokine@tutanota.com>
2024-08-09 14:39:54 +0200

    Change the behaviour of launch_baseline to make more sense
    Now it will unpause the dag if it is paused instead of immediately
    triggering it, since this would launch 2 runs. If the DAG is
    unpaused it will trigger it.

--- training/scripts/launch_baseline.sh
+++ training/scripts/launch_baseline.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 echo "Launching baseline training. This may take some time, especially to preprocess the data"
 # Trigger the appropriate dag
-curl -X POST 'localhost:8080/api/v1/dags/baseline/dagRuns' -H 'Content-Type: application/json' --user "admin:admin" -d '{}'
+# First check if it is paused, if so, unpause it. Otherwise manually trigger it
+# This avoid a "double run" that happens if you manually trigger a scheduled run
+
+res=$(curl -s -X GET 'localhost:8080/api/v1/dags/baseline' --user "admin:admin" -H 'Content-Type: application/json' | jq .is_paused)
+if [ "$res" = "true" ]; then
+    echo "Unpaused the dag"
+    curl -s -X PATCH 'localhost:8080/api/v1/dags/baseline?update_mask=is_paused' \
+        -H 'Content-Type: application/json' \
+        --user "admin:admin" \
+        -d '{
+            "is_paused": false
+        }'
+else
+    echo "Manually triggered the dag"
+    curl -s -X POST 'localhost:8080/api/v1/dags/baseline/dagRuns' -H 'Content-Type: application/json' --user "admin:admin" -d '{}' > /dev/null
+fi
+
+echo "View progress on http://localhost:8080"
