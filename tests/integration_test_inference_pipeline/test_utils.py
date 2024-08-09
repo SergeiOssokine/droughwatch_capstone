1921a9a87c278b093fd0d7ca33dcb72aa3463420
Serguei Ossokine <serguei.ossokine@tutanota.com>
2024-08-09 16:44:05 +0200

    Minor linting

--- tests/integration_test_inference_pipeline/test_utils.py
+++ tests/integration_test_inference_pipeline/test_utils.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict
+
 import docker
 from omegaconf import DictConfig, OmegaConf
 from rich.columns import Columns
@@ -6,7 +8,17 @@ from rich.panel import Panel
 from rich.pretty import Pretty
 
 
-def launch_lambda_container(name: str, config: DictConfig):
+def launch_lambda_container(name: str, config: DictConfig) -> Any:
+    """Launch the lambda container and override the CMD to the
+    appropriate name specified by name.
+
+    Args:
+        name (str): The name of the CMD override
+        config (DictConfig): The configuration dict
+
+    Returns:
+        Any: The container
+    """
     client = docker.from_env()
     container = client.containers.run(
         config.image,
@@ -18,12 +30,24 @@ def launch_lambda_container(name: str, config: DictConfig):
     return container
 
 
-def clean_up(container):
+def clean_up(container) -> None:
+    """Stop and remove the given docker container
+
+    Args:
+        container (docker.container): The container to clean up
+    """
     container.stop()
     container.remove()
 
 
-def print_difference(expected, received):
+def print_difference(expected: Dict[str, Any], received: Dict[str, Any]) -> None:
+    """Pretty print the 2 dictionaries side-by-side so it's easier to see
+    any differences
+
+    Args:
+        expected (Dict[str,Any]): The expected dict result
+        received (Dict[str,Any]): The actual result
+    """
     panel1 = Panel(Pretty(expected), title="Expected", expand=False)
     panel2 = Panel(Pretty(received), title="Received", expand=False)
 
