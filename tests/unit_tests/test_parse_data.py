62a1f1489a540e414c2b84270870079ab5119daa
Serguei Ossokine <serguei.ossokine@tutanota.com>
2024-08-09 23:24:25 +0200

    Add another parse_data test

--- tests/unit_tests/test_parse_data.py
+++ tests/unit_tests/test_parse_data.py
@@ -2,7 +2,7 @@ import os
 
 from deepdiff import DeepDiff
 
-from training.airflow.includes.parse_data import read_raw_tfrecord
+from training.airflow.includes.parse_data import add_derived_features, read_raw_tfrecord
 
 mpath = os.path.dirname(__file__)
 
@@ -12,11 +12,25 @@ raw_record = os.path.join(
 
 
 def test_parse_raw_record():
+    """
+    Test that parsing the raw TFRecord returns sensible results
+    """
     res = read_raw_tfrecord(raw_record)
     result = list(res.element_spec[0].keys())
     expected = ["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "B11"]
+    # Check that the dataset is Tuple(Dict,TensorSpec)
     assert len(res.element_spec) == 2
+    # Check that the arguments of the dict are as expected
     assert not DeepDiff(result, expected)
-    print(res.element_spec[0]["B1"].shape)
+    # Check that the shape of every Tensor is correct
     for key in expected:
         assert res.element_spec[0][key].shape == (65, 65, 1)
+
+
+def test_add_derived_features():
+    raw_dataset = read_raw_tfrecord(raw_record)
+    updated_dataset = raw_dataset.map(add_derived_features)
+    assert len(updated_dataset.element_spec) == 2
+    feats = updated_dataset.element_spec[0].keys()
+    for feature in ['NDVI', 'NDMI', 'EVI']:
+        assert feature in feats
